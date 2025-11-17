e8edd8e619fdc5c4bc1cff52fc04ee083ef7109a
Dilusha Sindutharu <sindutharu821@gmail.com>
2025-11-17 23:37:39 +0000

    Moving Burial API methods to Controller - 2

--- controllers/burialController.js
+++ controllers/burialController.js
@@ -1,4 +1,29 @@
 const Burial = require("../models/burialModel");
+const mongoose = require("mongoose");
+
+// get all burial records
+const getBurials = async (req, res) => {
+  try {
+    const burials = await Burial.find({}).sort({createdAt: -1});
+    res.status(200).json(burials);
+  } catch (error) {
+    res.status(400).json({error: error.message});
+  }
+};
+
+// get a single burial record by ID
+const getBurial = async (req, res) => {
+  const {id} = req.params;
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(404).json({error: "No such burial record ID"});
+  }
+  const burial = await Burial.findById(id);
+  if (!burial) {
+    return res.status(404).json({error: "Burial record not found"});
+  }
+
+  res.status(200).json(burial);
+};
 
 // Adding a new burial record
 const createBurial = async (req, res) => {
@@ -27,4 +52,46 @@ const createBurial = async (req, res) => {
   }
 };
 
-module.exports = {createBurial};
+//delete a burial record
+const deleteBurial = async (req, res) => {
+  const {id} = req.params;
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(404).json({error: "No such burial record ID"});
+  }
+  const burial = await Burial.findOneAndDelete({_id: id});
+  if (!burial) {
+    return res.status(404).json({error: "Burial record not found"});
+  }
+
+  res.status(200).json(burial);
+};
+
+//edit a burial record
+const editBurial = async (req, res) => {
+  const {id} = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(404).json({error: "No such burial record ID"});
+  }
+
+  const burial = await Burial.findOneAndUpdate(
+    {_id: id},
+    {
+      ...req.body,
+    }
+  );
+
+  if (!burial) {
+    return res.status(400).json({error: "Burial record not found"});
+  }
+
+  res.status(200).json(burial);
+};
+
+module.exports = {
+  createBurial,
+  getBurials,
+  getBurial,
+  deleteBurial,
+  editBurial,
+};
--- routes/burials.js
+++ routes/burials.js
@@ -1,75 +1,27 @@
 const express = require("express");
-const {createBurial} = require("../controllers/burialController");
+const {
+  createBurial,
+  getBurials,
+  getBurial,
+  deleteBurial,
+  editBurial,
+} = require("../controllers/burialController");
+
 const router = express.Router();
 
 //Get all burial records
-router.get("/", async (req, res) => {
-  try {
-    const burials = await Burial.find({}).sort({createdAt: -1});
-    res.status(200).json(burials);
-  } catch (error) {
-    res.status(400).json({error: error.message});
-  }
-});
+router.get("/", getBurials);
 
 // Adding a new burial record
 router.post("/", createBurial);
 
-// Edit a single burial record by ID
-router.patch("/:id", async (req, res) => {
-  const {id} = req.params;
+// Get a single burial record
+router.get("/:id", getBurial);
 
-  try {
-    const updatedBurial = await Burial.findByIdAndUpdate(
-      id,
-      {...req.body},
-      {new: true, runValidators: true}
-    );
-
-    if (!updatedBurial) {
-      return res.status(404).json({error: "Burial record not found"});
-    }
-
-    res.status(200).json(updatedBurial);
-  } catch (error) {
-    res.status(400).json({error: error.message});
-  }
-});
+// Edit a single burial record
+router.patch("/:id", editBurial);
 
 // Delete a single burial record
-router.delete("/:id", async (req, res) => {
-  const {id} = req.params;
+router.delete("/:id", deleteBurial);
 
-  try {
-    const deletedBurial = await Burial.findByIdAndDelete(id);
-
-    if (!deletedBurial) {
-      return res.status(404).json({error: "Burial record not found"});
-    }
-
-    res.status(200).json({
-      message: "Burial record deleted successfully",
-      deletedBurial,
-    });
-  } catch (error) {
-    res.status(400).json({error: error.message});
-  }
-});
-
-// Get a single burial record by ID
-router.get("/:id", async (req, res) => {
-  const {id} = req.params;
-
-  try {
-    const burial = await Burial.findById(id);
-
-    if (!burial) {
-      return res.status(404).json({error: "Burial record not found"});
-    }
-
-    res.status(200).json(burial);
-  } catch (error) {
-    res.status(400).json({error: error.message});
-  }
-});
 module.exports = router;
