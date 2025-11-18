55457b157fd36fc87a3123714fbfd3f4a66f18c6
Dilusha Sindutharu <sindutharu821@gmail.com>
2025-11-18 10:24:52 +0000

    Adding marriage model

---
+++ models/marriageModel.js
@@ -0,0 +1,24 @@
+const mongooese = require("mongoose");
+
+const Schema = mongooese.Schema;
+
+const marriageSchema = new Schema(
+  {
+    nameOfBride: {type: String, required: true},
+    nameOfGroom: {type: String, required: true},
+    dateOfMarriage: {type: Date, required: true},
+    timeOfMass: {type: String, required: true},
+    shortenedCoupleName: {type: String, required: true},
+    massType: {
+      type: String,
+      required: true,
+      enum: ["Full", "Half"],
+      default: "",
+    },
+    needChurchChoir: {type: Boolean, required: true},
+    useChurchDecos: {type: Boolean, required: false},
+  },
+  {timestamps: true}
+);
+
+module.exports = mongooese.model("Marriage", marriageSchema);
