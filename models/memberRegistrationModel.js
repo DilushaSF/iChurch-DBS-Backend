5577cf07d808b0b11ce6234063e4de0c298da42a
Dilusha Sindutharu <sindutharu821@gmail.com>
2025-11-23 18:08:11 +0000

    Adding Member Registration model.js file

---
+++ models/memberRegistrationModel.js
@@ -0,0 +1,41 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const childSchema = new Schema({
+    nameOfChild: { type: String, required: true },
+    dateOfBirthChild: { type: Date, required: true },
+    baptisedDateOfChild: { type: Date, required: false },
+    baptisedChurchOfChild: { type: String, required: false }
+}, { _id: true });
+
+
+const memberRegistrationSchema = new Schema(
+    {
+        church: { type: String, required: true },
+
+        nameOfFather: { type: String, required: true },
+        occupationOfFather: { type: String, required: false },
+        dateOfBirthFather: { type: Date, required: false },
+        baptisedDateOfFather: { type: Date, required: false },
+        baptisedChurch: { type: String, required: false },
+
+        nameOfMother: { type: String, required: true },
+        occupationOfMother: { type: String, required: false },
+        dateOfBirthOfMother: { type: Date, required: false },
+        baptisedDateOfMother: { type: Date, required: false },
+
+        address: { type: String, required: true },
+        contactNo: { type: String, required: true },
+
+        marriedDate: { type: Date, required: false },
+        marriedChurch: { type: String, required: false },
+
+        capableDonationPerMonth: { type: Number, required: false },
+
+        children: [childSchema]
+    },
+    { timestamps: true }
+);
+
+module.exports = mongoose.model("MemberRegistration", memberRegistrationSchema);
