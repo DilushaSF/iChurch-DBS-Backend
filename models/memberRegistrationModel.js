f854b940b93b61f7c1a2d2bd390d892c5338f0a4
Dilusha Sindutharu <sindutharu821@gmail.com>
2025-11-24 21:29:15 +0000

    Fixing Member Registration model attributes

--- models/memberRegistrationModel.js
+++ models/memberRegistrationModel.js
@@ -2,40 +2,43 @@ const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
-const childSchema = new Schema({
-    nameOfChild: { type: String, required: true },
-    dateOfBirthChild: { type: Date, required: true },
-    baptisedDateOfChild: { type: Date, required: false },
-    baptisedChurchOfChild: { type: String, required: false }
-}, { _id: true });
-
+const childSchema = new Schema(
+  {
+    nameOfChild: {type: String, required: true},
+    dateOfBirthChild: {type: Date, required: true},
+    baptisedDateOfChild: {type: Date, required: false},
+    baptisedChurchOfChild: {type: String, required: false},
+  },
+  {_id: true}
+);
 
 const memberRegistrationSchema = new Schema(
-    {
-        church: { type: String, required: true },
+  {
+    church: {type: String, required: true},
 
-        nameOfFather: { type: String, required: true },
-        occupationOfFather: { type: String, required: false },
-        dateOfBirthFather: { type: Date, required: false },
-        baptisedDateOfFather: { type: Date, required: false },
-        baptisedChurch: { type: String, required: false },
+    nameOfFather: {type: String, required: true},
+    occupationOfFather: {type: String, required: false},
+    dateOfBirthFather: {type: Date, required: false},
+    baptisedDateOfFather: {type: Date, required: false},
+    baptisedChurchOfFather: {type: String, required: false},
 
-        nameOfMother: { type: String, required: true },
-        occupationOfMother: { type: String, required: false },
-        dateOfBirthOfMother: { type: Date, required: false },
-        baptisedDateOfMother: { type: Date, required: false },
+    nameOfMother: {type: String, required: true},
+    occupationOfMother: {type: String, required: false},
+    dateOfBirthOfMother: {type: Date, required: false},
+    baptisedDateOfMother: {type: Date, required: false},
+    baptisedChurchOfMother: {type: String, required: false},
 
-        address: { type: String, required: true },
-        contactNo: { type: String, required: true },
+    address: {type: String, required: true},
+    contactNo: {type: String, required: true},
 
-        marriedDate: { type: Date, required: false },
-        marriedChurch: { type: String, required: false },
+    marriedDate: {type: Date, required: false},
+    marriedChurch: {type: String, required: false},
 
-        capableDonationPerMonth: { type: Number, required: false },
+    capableDonationPerMonth: {type: Number, required: false},
 
-        children: [childSchema]
-    },
-    { timestamps: true }
+    children: [childSchema],
+  },
+  {timestamps: true}
 );
 
 module.exports = mongoose.model("MemberRegistration", memberRegistrationSchema);
